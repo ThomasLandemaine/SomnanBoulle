2afab06f8b37489ad7be39da2e89099008b1f22c
Etienne LEMIERE <etienne@MacBook-Pro-de-Etienne.local>
2022-11-25 20:57:17 +0100

    feat: noice

--- main.py
+++ main.py
@@ -45,10 +45,11 @@ mp_holistic = mp.solutions.holistic
 cap = cv2.VideoCapture(0) # Number is to find the good webcam
 
 # Initiate holistic Model
+black = np.zeros([20, 20, 1], dtype="uint8")
 white = np.full((500, 500, 3), 255, dtype=np.uint8)
-drawer = cv2.cvtColor(white, cv2.COLOR_RGB2BGR)
-drawer = cv2.resize(drawer, (1200, 1000))
-i = 0
+drawer = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
+drawer = cv2.resize(drawer, (1600, 900))
+cv2.resizeWindow('drawer', (1600, 900))
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while (cap.isOpened()):
         sequence = 0
@@ -56,68 +57,61 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             
             time_end = time.time() + 10
             while (time.time() < time_end):
-                
                 ret, frame = cap.read()
 
                 #recolor feed
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 #Make detection
                 results = holistic.process(image)
-                # print(results.pose_landmarks) # Pour le corps ?
-                # print(results.left_hand_landmarks)
-                # print(results.right_hand_landmarks)
-                #mp_holistic.HAND_CONNECTIONS = lancer cette ligne pour savoir ce qu'il se passe. On peut mettre des if ou des = à qui déclenche tel ou tel truc
-                # print(results.face_landmarks)
-
-                # Recolor image back to BGR for rendering
-
-                #draw face landmarks
-                #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
 
                 #Right Hand
+                connections_wanted = frozenset(
+                    [(16, 18), (14, 16), (12, 14), (11, 12), (11, 13), (13, 15), (15, 17), (24, 26), (26, 28), (28, 30),
+                     (30, 32), (23, 25), (25, 27), (27, 29), (29, 31)])
+                # Right Hand
                 mp_drawing.draw_landmarks(
                     image=drawer,
                     landmark_list=results.right_hand_landmarks,
-                    connections=mp_holistic.HAND_CONNECTIONS,
+                    connections=mp_holistic.HAND_CONNECTIONS.intersection(connections_wanted),
                     landmark_drawing_spec=mp_drawing.DrawingSpec(
                         color=(255, 255, 255),
-                        thickness=-50,
+                        thickness=0,
                         circle_radius=0,
                     )
-                    )
-                #Left Hand
+                )
+                # Left Hand
                 mp_drawing.draw_landmarks(
                     image=drawer,
                     landmark_list=results.left_hand_landmarks,
-                    connections=mp_holistic.HAND_CONNECTIONS,
+                    connections=mp_holistic.HAND_CONNECTIONS.intersection(connections_wanted),
                     landmark_drawing_spec=mp_drawing.DrawingSpec(
                         color=(255, 255, 255),
-                        thickness=-50,
+                        thickness=0,
                         circle_radius=0,
                     )
-                    )
-                #Pose detection
+                )
+                # Pose detection
                 mp_drawing.draw_landmarks(
                     image=drawer,
                     landmark_list=results.pose_landmarks,
-                    connections=mp_holistic.POSE_CONNECTIONS,
+                    connections=mp_holistic.POSE_CONNECTIONS.intersection(connections_wanted),
                     landmark_drawing_spec=mp_drawing.DrawingSpec(
                         color=(255, 255, 255),
-                        thickness=-50,
+                        thickness=0,
                         circle_radius=0,
                     )
-                    )
-                cv2.imshow('Raw Webcam Feed', drawer)
-                drawer = cv2.resize(drawer, (1000, 800))
+                )
+                cv2.imshow('drawer', drawer)
+                drawer = cv2.resize(drawer, (1600, 900))
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
                 # STOCKER LA SEQUENCE ISOLEE
-            time.sleep(20)
+            time.sleep(5)
+            drawer = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
             sequence += 1
-            drawer = cv2.cvtColor(white, cv2.COLOR_RGB2BGR)
             #if sequence == 5:
                 # STOCKER SOMME DES 5 SEQUENCES
-        #i += 1
+
 
 cap.release()
 cv2.destroyAllWindows()
