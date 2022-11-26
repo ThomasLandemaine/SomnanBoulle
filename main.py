6b331752c3c18df4a865d90eaf11bd776bd6d984
Thomas Landemaine <thomas@armis.tech>
2022-11-26 20:47:06 +0100

    feat: add

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # https://www.youtube.com/watch?v=pG4sUNDOZFg
 
 import cv2
-import matplotlib
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 import mediapipe as mp
 import time
+from playsound import playsound
 from matplotlib import pyplot as plt
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 cap = cv2.VideoCapture(0)  # Number is to find the good webcam
+
 
 LILA = (243, 79, 124)
 LIGHT_LILA = (248, 151, 177)
@@ -32,22 +32,24 @@ light_color_tab = [LIGHT_LILA, LIGHT_GREEN, LIGHT_ORANGE, LIGHT_BLUE, LIGHT_PINK
 black = np.zeros([20, 20, 1], dtype="uint8")
 white = np.full((500, 500, 3), 255, dtype=np.uint8)
 drawer = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
-drawer = cv2.resize(drawer, (1600, 900))
+drawer = cv2.resize(drawer, (1920, 1000))
 
 drawer_shoulders = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
-drawer_shoulders = cv2.resize(drawer, (1600, 900))
+drawer_shoulders = cv2.resize(drawer, (1920, 1000))
 drawer_right_hand = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
-drawer_right_hand = cv2.resize(drawer, (1600, 900))
+drawer_right_hand = cv2.resize(drawer, (1920, 1000))
 drawer_left_hand = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
-drawer_left_hand = cv2.resize(drawer, (1600, 900))
+drawer_left_hand = cv2.resize(drawer, (1920, 1000))
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     session = 0
     while (cap.isOpened()):
         sequence = 0
         while (sequence < 5):
-
             picture_save = 'picture' + str(session) + '_' + str(sequence) + '.png'
+            picture_save2 = 'picture' + str(session) + '_' + str(sequence)
+            
+            playsound('sound.mp3', False)
             time_end = time.time() + 10
             i = 0
             while (time.time() < time_end):
@@ -136,21 +138,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                         circle_radius=0,
                     )
                 )
-                # mp_drawing.draw_landmarks(
-                #     image=drawer_shoulders,
-                #     landmark_list=results.pose_landmarks,
-                #     connections=mp_holistic.POSE_CONNECTIONS.intersection(connection_shoulders),
-                #     landmark_drawing_spec=mp_drawing.DrawingSpec(
-                #         color=LIGHT_LILA,
-                #         thickness=0,
-                #         circle_radius=0,
-                #     ),
-                #     connection_drawing_spec=mp_drawing.DrawingSpec(
-                #         color=LILA,
-                #         thickness=1,
-                #         circle_radius=0,
-                #     )
-                # )
                 if (i > 50):
                     drawer_right_hand = cv2.GaussianBlur(drawer_right_hand, (3, 3), 0)
                     drawer_left_hand = cv2.GaussianBlur(drawer_left_hand, (3, 3), 0)
@@ -158,6 +145,9 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 print(i)
                 drawer = cv2.addWeighted(drawer, 1, drawer_right_hand, 0.1, 0)
                 drawer = cv2.addWeighted(drawer, 1, drawer_left_hand, 0.1, 0)
+                cv2.namedWindow('drawer', cv2.WND_PROP_FULLSCREEN)
+                cv2.setWindowProperty('drawer', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                cv2.putText(drawer, picture_save2, (20,20), cv2.FONT_HERSHEY_DUPLEX, 1, light_color_tab[sequence])
                 cv2.imshow('drawer', drawer)
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
@@ -166,21 +156,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             
             
             drawer = cv2.imread('black.png')
-            drawer_shoulders = cv2.imread('black.png')
             drawer_right_hand = cv2.imread('black.png')
             drawer_left_hand = cv2.imread('black.png')
-            
-            
-            drawer = cv2.resize(drawer, (1600, 900))
-            drawer_shoulders = cv2.resize(drawer_shoulders, (1600, 900))
-            drawer_right_hand = cv2.resize(drawer_right_hand, (1600, 900))
-            drawer_left_hand = cv2.resize(drawer_left_hand, (1600, 900))
+            drawer = cv2.resize(drawer, (1920, 1000))
+            drawer_right_hand = cv2.resize(drawer_right_hand, (1920, 1000))
+            drawer_left_hand = cv2.resize(drawer_left_hand, (1920, 1000))
             
             
             sequence += 1
         session += 1
-        # if sequence == 5:
-        # STOCKER SOMME DES 5 SEQUENCES
-
 cap.release()
 cv2.destroyAllWindows()
