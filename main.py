8c376ff8ec53e72f3bc97fcc08a60dadbf1776c6
Etienne LEMIERE <etienne@MacBook-Pro-de-Etienne.local>
2022-11-26 15:11:40 +0100

    test: test

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def angle(a,b,c):
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
-cap = cv2.VideoCapture(-1) # Number is to find the good webcam
+cap = cv2.VideoCapture(0) # Number is to find the good webcam
 
 LILA = (243, 79, 124)
 LIGHT_LILA = (248, 151, 177)
@@ -53,6 +53,8 @@ black = np.zeros([20, 20, 1], dtype="uint8")
 white = np.full((500, 500, 3), 255, dtype=np.uint8)
 drawer = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
 drawer = cv2.resize(drawer, (1600, 900))
+drawer_shoulders = cv2.cvtColor(black, cv2.COLOR_RGB2BGR)
+drawer_shoulders = cv2.resize(drawer, (1600, 900))
 #tab_rgb = {BLUE, RED, }
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     session = 0
@@ -72,8 +74,9 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
                 #Right Hand
                 connections_wanted = frozenset(
-                    [(16, 18), (14, 16), (12, 14), (11, 12), (11, 13), (13, 15), (15, 17), (24, 26), (26, 28), (28, 30),
+                    [(16, 18), (14, 16), (12, 14), (11, 13), (13, 15), (15, 17), (24, 26), (26, 28), (28, 30),
                      (30, 32), (23, 25), (25, 27), (27, 29), (29, 31)])
+                connection_shoulders = frozenset([(11, 12)])
                 # Right Hand
                 mp_drawing.draw_landmarks(
                     image=drawer,
@@ -110,6 +113,23 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                         circle_radius=0,
                     )
                 )
+                mp_drawing.draw_landmarks(
+                    image=drawer_shoulders,
+                    landmark_list=results.pose_landmarks,
+                    connections=mp_holistic.POSE_CONNECTIONS.intersection(connection_shoulders),
+                    landmark_drawing_spec=mp_drawing.DrawingSpec(
+                        color=LIGHT_LILA,
+                        thickness=0,
+                        circle_radius=0,
+                    ),
+                    connection_drawing_spec=mp_drawing.DrawingSpec(
+                        color=LILA,
+                        thickness=1,
+                        circle_radius=0,
+                    )
+                )
+                drawer_shoulders = cv2.GaussianBlur(drawer_shoulders, (5, 5), 0)
+                drawer = cv2.vconcat(drawer, drawer_shoulders)
                 cv2.imshow('drawer', drawer)
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
@@ -117,6 +137,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             cv2.imwrite(picture_save, drawer)
             drawer = cv2.imread('black.png')
             drawer = cv2.resize(drawer, (1600, 900))
+            #drawer = cv2.resize(drawer, (1600, 900))
             sequence += 1
         session += 1
             #if sequence == 5:
